56bc7788cddaa3603baf36f920edb82073a164eb
Teresa Plaz <teresaplaz@gmail.com>
2018-04-23 01:05:09 -0400

    inserting content

---
+++ js/index.js
@@ -0,0 +1,5 @@
+$('ul li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).fadeOut(500);
+});
