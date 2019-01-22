1526ed875ae6bcb41b4eb90704cc3bf1de604803
Pol Goma <polgoide@gmail.com>
2019-01-21 19:07:59 -0600

    done

---
+++ starter-code/main.js
@@ -0,0 +1,7 @@
+let carousels = bulmaCarousel.attach();
+let carouselPlace = document.getElementById("carouselPlace")
+carouselPlace.appendChild(carousel)
+
+window.onload(() =>  {
+  var carousels = bulmaCarousel.attach();
+})
