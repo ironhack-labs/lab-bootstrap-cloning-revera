1d7b1aaa1b24be21ce3556d2572d1907f0b437da
Upward-Spiral <azadeh.farshidi@gmail.com>
2020-02-28 16:21:32 +0100

    done as much as I could :|

---
+++ starter-code/js/main.js
@@ -0,0 +1,9 @@
+// let $togglingMenu = document.querySelector(".nav-item.dropdown");
+// let $parent = document.querySelector(".dropdown-toggle");
+// let $grandParent = document.querySelector(".dropdown")
+// let showList = "show";
+// $grandParent.addEventListener("mouseenter",function(){
+//     $togglingMenu.setAttribute("class", "dropdown-menu show");
+//     $parent.setAttribute("area-expanded", "true")
+//     $grandParent.setAttribute("class", "nav-item dropdown show");
+// })
