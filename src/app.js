1565548fd301934cc8dc4b660b06394e953735fb
Old <onelifedesigning@gmail.com>
2020-05-26 17:49:15 +0200

    web pack install

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import 'bootstrap'
+import './scss/app.scss'
+import ProjectOne from './img/project-1.jpg'
+const myProjectOne = new Image()
+myProjectOne.src = ProjectOne
+
+document.body.appendChild(myProjectOne)
