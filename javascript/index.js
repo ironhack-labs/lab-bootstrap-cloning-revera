bb4d024784c4ace540d1fde2b3c097b997e2b288
Marcos Salvo <marcossalvo95@gmail.com>
2021-05-09 18:25:48 +0200

    Almost finished

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,14 @@
 const footerLinks = document.querySelectorAll("#upper-footer a");
 
-footerLinks.addEventListener ("mouseover", () => {
-    footerLinks.style.color = "white";
-})
+
+for (let i = 0; i< footerLinks.length; i++) {
+    footerLinks[i].addEventListener ("mouseover", (event) => {
+        event.target.style.color = "white";
+    })
+}
+
+for (let i = 0; i< footerLinks.length; i++) {
+    footerLinks[i].addEventListener ("mouseout", (event) => {
+        event.target.style.color = "rgb(187, 179, 179)";
+    })
+}
